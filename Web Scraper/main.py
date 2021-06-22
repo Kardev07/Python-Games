07d2d723872937b1ea684d7f6b3e10420f0845ab
Karthik Pariti <77888951+Kardev07@users.noreply.github.com>
2021-06-22 20:45:55 +0530

    Add files via upload

---
+++ Web Scraper/main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_user = input("Enter a GitHub user: ")
+
+url = "https://github.com/" + github_user
+
+r = requests.get(url)
+
+soup = bs(r.content, 'html.parser')
+
+profile_pic = soup.find("img", {"alt" : "Avatar"})["src"]
+
+print(profile_pic)
